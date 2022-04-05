bd9e83313dece87d21d37e1461a14ff8434a8ab5
Aleksej Malekov <malekovaleksej@gmail.com>
2022-04-05 16:38:33 +0500

    i've got how to use router to get static files)

--- src/mixology.js
+++ src/mixology.js
@@ -1,22 +1,54 @@
 const express = require('express');
 let app = express();
-app.use(express.static(__dirname + "/public"));
 
 const Mixology_Router = express.Router();
 
 // page information
-const PAGE_INFO = require()
+const PAGE_INFO = require("./public/pageRender/pageinfo.js")
 
 
-
+Mixology_Router.use(express.static(__dirname + "/public"));
 Mixology_Router.get("/" , (req,res) => {
-    res.render("mixology.hbs")
-})
-// Mixology_Router.get("/fresh_taste" , (req,res) => {
     
-//     res.render("mixology.hbs" , {
-
-//         content : MIXOLOGY.mixology
-//     })
-// })
+    res.render("mixology" , {
+        title : PAGE_INFO.mixology.title ,
+        content : "some text"
+    });
+});
+Mixology_Router.get("/fresh_taste" , (req,res) => {
+    res.render("mixology" , {
+        title : PAGE_INFO.mixology.title,
+        content : PAGE_INFO.mixology.content.fresh
+    })
+});
+Mixology_Router.get("/tart_taste" , (req,res) => {
+    res.render("mixology" , {
+        title : PAGE_INFO.mixology.title,
+        content : PAGE_INFO.mixology.content.tart
+    })
+})
+Mixology_Router.get("/sweet_taste" , (req,res) => {
+    res.render("mixology" , {
+        title : PAGE_INFO.mixology.title,
+        content : PAGE_INFO.mixology.content.sweet
+    })
+})
+Mixology_Router.get("/spicy_taste" , (req,res) => {
+    res.render("mixology" , {
+        title : PAGE_INFO.mixology.title,
+        content : PAGE_INFO.mixology.content.spicy
+    })
+})
+Mixology_Router.get("/dessert_taste" , (req,res) => {
+    res.render("mixology" , {
+        title : PAGE_INFO.mixology.title,
+        content : PAGE_INFO.mixology.content.dessert
+    })
+})
+Mixology_Router.get("/original_mixes" , (req,res) => {
+    res.render("mixology" , {
+        title : PAGE_INFO.mixology.title,
+        content : PAGE_INFO.mixology.content.original
+    })
+})
 module.exports = Mixology_Router;
