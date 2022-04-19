f340940c0edff1f6c1d2763b99e8b0f9f2b42a65
Aleksej Malekov <malekovaleksej@gmail.com>
2022-04-19 15:46:15 +0500

    Age validation was designed like OOP concept , but i have some doubts"

--- src/features/validateAge.js
+++ src/features/validateAge.js
@@ -1,14 +1,47 @@
-function validateAge () {
-    function allowAccess () {
-        // let url = window.location.href;
-        // fetch("/allowed" , {method : 'GET'}).then(res => {
-        //     console.log(res)
-        //     document.write(res.text());
-        // });
-        document.cookie = 'isAccess=true';
-        this.parentNode.parentNode.style.display = 'none';
+class setCookie {
+    constructor(){
+        let background = document.createElement('div');
+            background.classList.add("age-validator-back");
+        let border = document.createElement('div');
+            border.classList.add("age-validator");
+
+        let borderTitle = document.createElement("h1");
+            borderTitle.classList.add("age-validator-title")
+            borderTitle.innerHTML = "Are you 18 years old?";
+
+        let buttonAccept = document.createElement("input");
+            buttonAccept.setAttribute('type' , "button");
+            buttonAccept.setAttribute("value" , "Yes");
+            buttonAccept.classList.add("validator-btn");
+            buttonAccept.addEventListener("click" , this.allowAccess);
+
+        let buttonReject = document.createElement("input");
+            buttonReject.setAttribute('type' , "button");
+            buttonReject.setAttribute("value" , "No");
+            buttonAccept.classList.add("validator-btn");
+            buttonReject.addEventListener("click" , this.rejectAccess);
+        // put elements in modal windows 
+        border.append(borderTitle ,buttonAccept ,buttonReject);
+        background.append(border);
+        this.validator = background;
     }
-    function rejectAccess () {
+    createWindow = () => {
+        let cookieState = this.getCookie('isAccess');
+        if(!cookieState){
+            document.cookie = 'isAccess=false;';
+            console.log('isAccess is undefined')
+        }
+
+        if(cookieState === 'false'){
+            document.body.appendChild( this.validator );
+        }
+    }
+    allowAccess = (event) => {
+        let target = event.target;
+            target.parentNode.parentNode.style.display = 'none';
+        document.cookie = 'isAccess=true';
+    }
+    rejectAccess = () => {
         document.cookie = 'isAccess=false';
         fetch("/rejected" , {method : 'POST'})
         .then(res => {
@@ -18,55 +51,20 @@ function validateAge () {
             document.write(html)
         })
     }
-    let background = document.createElement('div');
-        background.classList.add("age-validator-back");
-    let border = document.createElement('div');
-        border.classList.add("age-validator");
-
-    let borderTitle = document.createElement("h1");
-        borderTitle.classList.add("age-validator-title")
-        borderTitle.innerHTML = "Are you 18 years old?";
-
-    let buttonAccept = document.createElement("input");
-        buttonAccept.setAttribute('type' , "button");
-        buttonAccept.setAttribute("value" , "Yes");
-        buttonAccept.classList.add("validator-btn");
-        buttonAccept.addEventListener("click" , allowAccess);
-
-    let buttonReject = document.createElement("input");
-        buttonReject.setAttribute('type' , "button");
-        buttonReject.setAttribute("value" , "No");
-        buttonAccept.classList.add("validator-btn");
-        buttonReject.addEventListener("click" , rejectAccess);
-    // put elements in modal windows 
-        border.append(borderTitle ,buttonAccept ,buttonReject);
-        background.append(border);
-
-    document.body.appendChild(background);
-};
-// cookie 
-function getCookie (cookie , cookieName) {
-    let value = cookie.split(";").filter( el => {
-        if(el.match(cookieName)){
-            return el;
-        };
-        
-    });
-    if(value[0]){
-        return value[0].trim().replace(cookieName + "=" , '')
-    }else{
-        return undefined;
+    getCookie = (cookieName) => {
+        let value = document.cookie.split(";").filter( el => {
+            if(el.match(cookieName)){
+                return el;
+            };
+            
+        });
+        if(value[0]){
+            return value[0].trim().replace(cookieName + "=" , '')
+        }else{
+            return undefined;
+        }
     }
 }
 
-let access = getCookie(document.cookie , "isAccess");
-console.log(typeof access)
-if( access === 'false'){
-
-    console.log("false")
-    validateAge();
-}else if( access === undefined){
-    console.log("undefffffffffff")
-    document.cookie = 'isAccess=false';
-}
-// let getAccess = getCookie(document.cookie , "isAccess");
+const cookies = new setCookie();
+cookies.createWindow();
