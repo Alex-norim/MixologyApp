382d717100b8414dc093fa8457ff391cab6d5fe4
Aleksej Malekov <malekovaleksej@gmail.com>
2022-04-22 00:43:14 +0500

    there is created json file in savedSqlRequestFolder if company names don't exist

--- src/tabacoRouter.js
+++ src/tabacoRouter.js
@@ -1,27 +1,68 @@
 const express = require('express');
-
+const mysql = require('mysql');
+const fs = require('fs');
 const TabacoRouter = express.Router();
 
-const PAGE_INFO = require("./public/pageRender/pageinfo.js")
-const CONTENT = PAGE_INFO.tabaco.content;
-const connection = require('./features/mysql_connection');
+const content = {
+    title : "Tabaco" ,
+    value : "some text of tabaco sort",
+    text : "some rext",
+    menuLinks : [
+        { name : 'Home' , link : '/'},
+        { name : 'Mixology' , link : '/mixology'},
+        { name : 'Tabaco' , link : '/tabaco'},
+        { name : 'About project' , link : '/project'},
+    ],
+    content : 'As a fun of hookah I prefer listed brands of tabaco you see in the left bar. You can get some interesting things about the company who produces this brand , origin and other details'
+};
 
+
+const config = require('./features/mysql_connection');
+
+const pathToSavedRequests = './src/savedSqlRequests/';
+TabacoRouter.use( (req,res,next) => {
+    next()
+})
 TabacoRouter.get("/" , (req,res) => {
-    res.render("tabaco.hbs" , PAGE_INFO.tabaco );
+    let tabacoCompanyes = fs.existsSync(pathToSavedRequests + 'companyNames.json');
+
+    if(!tabacoCompanyes){
+        const getCompanyNames = `SELECT DISTINCT company FROM brand`;
+        const connection = mysql.createConnection(config);
+        connection.connect();
+        connection.query(getCompanyNames , (err, row) => {
+            if(err){
+                console.log(err)
+            }else{
+                fs.writeFileSync( pathToSavedRequests + 'companyNames.json' , JSON.stringify(row));
+                let dt = row.map(el=> el.company);
+                res.render("tabaco.hbs" , {
+                    ...content,
+                    tabacoMaker : dt,
+                });
+            }
+        })
+        connection.end();
+    }else{
+        let names = fs.readFileSync(pathToSavedRequests + 'companyNames.json');
+        let parsedCompanyNames = JSON.parse(names).map(el => el.company);
+        res.render("tabaco.hbs" , {
+            ...content,
+            tabacoMaker : parsedCompanyNames,
+        });
+    }
 })
 
-TabacoRouter.post("/get_brands" , (req,res) => {
-    connection.connect( err => {
-        (err)?console.log(err):console.log("success");
-    })
-
-    connection.end( err => {
-        (err)?console.log(err):console.log("closed")
-    })
-
+TabacoRouter.get('/burn' , (req,res) => {
+    const Connection = mysql.createConnection(config);
+    Connection.connect();
+    const query = `SELECT `;
     res.render('tabaco' , {
-        ...PAGE_INFO.tabaco ,
-        text : "some rext"
+        ...content,
+        
     })
+    Connection.end()
 })
+
+
 module.exports = TabacoRouter; 
