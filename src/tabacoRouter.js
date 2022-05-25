0ab4ecc106fe0ca34a4e2bafa44553f090b7e4a0
Aleksej Malekov <malekovaleksej@gmail.com>
2022-05-25 16:14:46 +0500

    tabacoRouter had been also ridded of boilerplate code

--- src/tabacoRouter.js
+++ src/tabacoRouter.js
@@ -1,29 +1,30 @@
 const express = require('express');
-
+const fs = require('fs');
+const { __express } = require('hbs');
 const TabacoRouter = express.Router();
 const PAGE_INFO = require("./public/pageRender/pageinfo.js").tabaco;
 
+// middleware functions
+
+// some functions
 async function getCompanynames(sqlReq){
     const mysql = require('mysql2');
     const mysqlConfig = require('./features/mysql_connection');
-    let data = 'can not reach server';
+    
     let conn = await mysql.createConnection(mysqlConfig).promise();
-    data = await conn.execute(sqlReq).then( ([row, field]) => {
+    let data = await conn.execute(sqlReq).then( ([row, field]) => {
         return row;
     }).then( result => {
         conn.end()
         return result.filter( el => el.company )
     }).catch(err => {
-        console.log('tr conn is not est');
+        return ['Server not found']
     })
     // await conn.end();
     return data;
 }
-// middleware functions
-TabacoRouter.use('/' , (req,res,next) => {
-    
-    next()
-})
+
+
 
 // common requests
 TabacoRouter.get("/" , (req,res) => {
@@ -43,40 +44,19 @@ TabacoRouter.get("/" , (req,res) => {
     })
 })
 
-TabacoRouter.get('/burn' , (req,res) => {
+TabacoRouter.get(/burn|darkside|armango|musthave/ , (req,res) => {
+    let name = req.url.slice(1);
+    fs.readFile('src/jsonFiles/companyHistory.json' ,'utf-8' , (err , data) => {
+        if(err){
+            throw err
+        }else{
+            let hist = JSON.parse(data)[name];
+            res.send(JSON.stringify({
+                res : hist
+            }))
+        }
+    });
     
-    
-    res.render('tabaco' , {
-        ...PAGE_INFO,
-        tabacoMaker : parsedCompanyNames,
-        content : ''
-    })
-})
-
-TabacoRouter.get('/darkside' , (req,res) => {
-    res.render('tabaco' , {
-        ...PAGE_INFO,
-        tabacoMaker : parsedCompanyNames,
-        content : 'text[url]'
-    })
-})
-
-TabacoRouter.get('/armango' , (req,res) => {
-    
-    res.render('tabaco' , {
-        ...PAGE_INFO,
-        tabacoMaker : parsedCompanyNames,
-        content : 'text[url]'
-    })
-})
-
-TabacoRouter.get('/musthave' , (req,res) => {
-    
-    res.render('tabaco' , {
-        ...PAGE_INFO,
-        tabacoMaker : parsedCompanyNames,
-        content : 'text[url]'
-    })
 })
 
 
