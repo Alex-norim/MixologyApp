ff7f271c5a56af850035ba6253f32621630c1b64
Aleksej Malekov <malekovaleksej@gmail.com>
2022-08-02 23:21:22 +0500

    The user cabinet is initiated

---
+++ src/features/cabinet/cabinet.js
@@ -0,0 +1,9 @@
+import { Model } from "./model";
+import { View } from "./view";
+
+export const Cabinet = {
+    init : (_root) => {
+        const root = _root;
+        console.log('ddd')
+    }
+}
---
+++ src/features/cabinet/model.js
@@ -0,0 +1,6 @@
+import {Model as importModel} from '../mixology/model';
+export const Model = {
+    likeHandler : importModel.likeHandler,
+    getRecipeItems : importModel.showSpecificList,
+    getBestRecipe : importModel.getBestRecipes,
+}
